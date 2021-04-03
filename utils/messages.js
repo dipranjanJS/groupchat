4dc15e711ccc3608ba36794839efce0b7f538c01
dipranjanJS <dipranjan16oct@gmail.com>
2021-04-04 01:32:46 +0530

    message timestamp issue fix

--- utils/messages.js
+++ utils/messages.js
@@ -3,7 +3,7 @@ function formatMessage(username, text) {
     return {
         username,
         text,
-        time: moment().format('h:mm a')
+        time: moment(new Date().getTime()).format('h:mm a')
     }
 }
 
